011b80d23409e585aa9b5ea23bf184b66a88c4ab
Jaben <cheungjaben@hotmail.com>
2019-08-31 00:07:39 +0800

    update sample

--- ZBSToolsSample/ZBSToolsSample/ViewController.swift
+++ ZBSToolsSample/ZBSToolsSample/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var selectImageButton: UIButton!
+    @IBOutlet weak var previewImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func selectImageButton(_ sender: Any) {
+        
+        weak var weakSelf = self
+        
+        func loadImageFrom(_ source: ZBSImagePickerSource) {
+            ZBSImagePickerManager.shared.launchImagePickerIn(controller: weakSelf!, sourceType: source, didCompleted: { (image) in
+                weakSelf?.previewImageView.image = image
+            })
+        }
+        
+        self.presentSimpleActionSheetWith(name: "Get image", message: "From source",
+                                          buttons: ZBSButton(name: "Photo Library", action: { (action) in
+                                            loadImageFrom(.photoLibrary)
+                                          }, style: .destructive),
+                                          
+                                          ZBSButton(name: "Photo Albumn", action: { (action) in
+                                            loadImageFrom(.savedPhotosAlbum)
+                                          }, style: .default),
+                                          
+                                          ZBSButton(name: "Camera", action: { (action) in
+                                            loadImageFrom(.camera)
+                                          }, style: .default),
+                                          
+                                          ZBSButton(name: "Cancel", action: nil, style: .cancel)
+        )
+        
     }
 
 
